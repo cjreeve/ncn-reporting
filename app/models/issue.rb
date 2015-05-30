0611ef9765338e29d0218239fb215a3b19acbfc0
cjreeve <cjreeve@gmail.com>
2015-05-30 18:48:41 +0000

    add google test map

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,2 +1,10 @@
 class Issue < ActiveRecord::Base
+  
+  before_validation :set_issue_number
+  
+  private
+  
+  def set_issue_number
+    self.issue_number = Issue.last.issue_number + 1
+  end
 end
