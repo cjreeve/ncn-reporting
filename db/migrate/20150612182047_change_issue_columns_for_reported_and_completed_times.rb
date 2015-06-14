eb15b06df020ab20128653185a77b4382ab80ec6
Christopher Reeve <cjreeve@gmail.com>
2015-06-14 10:40:35 +0100

    fixes so fresh migrations and view works

--- db/migrate/20150612182047_change_issue_columns_for_reported_and_completed_times.rb
+++ db/migrate/20150612182047_change_issue_columns_for_reported_and_completed_times.rb
@@ -1,10 +1,10 @@
 class ChangeIssueColumnsForReportedAndCompletedTimes < ActiveRecord::Migration
-  def up
-    rename_column :issues, :time_reported, :reported_at
-    rename_column :issues, :time_completed, :completed_at
-  end
-  def down
-    rename_column :issues, :reported_at, :time_reported
-    rename_column :issues, :completed_at, :time_completed
-  end
+  # def up
+  #   rename_column :issues, :time_reported, :reported_at
+  #   rename_column :issues, :time_completed, :completed_at
+  # end
+  # def down
+  #   rename_column :issues, :reported_at, :time_reported
+  #   rename_column :issues, :completed_at, :time_completed
+  # end
 end
