34cafe736f757c609ca90529bf0b93f70ed9bbec
Christopher Reeve <cjreeve@gmail.com>
2015-06-23 23:22:58 +0100

    enable images to be saved through issues

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -137,6 +137,6 @@ class IssuesController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def issue_params
       params.require(:issue).permit(:issue_number, :title, :description, :priority, :reported_at,
-        :completed_at, :location_name, :coordinate, :route_id)
+        :completed_at, :location_name, :coordinate, :route_id, images_attributes: [:id, :url, :caption, :_destroy])
     end
 end
