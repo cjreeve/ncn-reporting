827ebf2987c696aa0b4a574a31c10d1e655e6944
Christopher Reeve <cjreeve@gmail.com>
2015-07-28 22:57:58 +0100

    log last user activity

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :log_user_activity
+
   rescue_from CanCan::AccessDenied do |exception|
     if user_session
       redirect_url_ = '/'
@@ -10,5 +12,11 @@ class ApplicationController < ActionController::Base
       redirect_url_ = user_session_url
     end
     redirect_to redirect_url_, :alert => exception.message
+  end
+
+  private
+
+  def log_user_activity
+    current_user.touch if current_user
   end
 end
