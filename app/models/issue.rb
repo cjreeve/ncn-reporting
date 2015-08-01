c8b9405f1fc40d6ff7d719adb0c40bf050ca0fda
Christopher Reeve <cjreeve@gmail.com>
2015-08-01 11:25:49 +0100

    add validation message to issue title

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -21,7 +21,7 @@ class Issue < ActiveRecord::Base
   before_validation :set_priority
   before_validation :set_title
 
-  validates :title, presence: true
+  validates :title, presence: { message: '- the problem is not defined'}
 
   after_validation :coordinate_to_latlng
 
