3745dcd4536935ffa40e179e0b7ccbb3bab08f12
Christopher Reeve <cjreeve@gmail.com>
2015-08-19 08:43:03 +0100

    update routes slug migration as can't have uniq slug that is the same

--- db/migrate/20150818215615_add_slug_to_routes.rb
+++ db/migrate/20150818215615_add_slug_to_routes.rb
@@ -1,6 +1,6 @@
 class AddSlugToRoutes < ActiveRecord::Migration
   def change
     add_column :routes, :slug, :string, null: false, default: ''
-    add_index :routes, :slug, unique: true
+    add_index :routes, :slug
   end
 end
