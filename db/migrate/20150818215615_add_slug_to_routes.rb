d88efce0c0c7abc1d76aa3ec9eb9708cd3c246e3
Christopher Reeve <cjreeve@gmail.com>
2015-08-19 08:56:24 +0100

    add unique index to routes slug

--- db/migrate/20150818215615_add_slug_to_routes.rb
+++ db/migrate/20150818215615_add_slug_to_routes.rb
@@ -1,6 +1,5 @@
 class AddSlugToRoutes < ActiveRecord::Migration
   def change
     add_column :routes, :slug, :string, null: false, default: ''
-    # add_index :routes, :slug, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150818215615) do
+ActiveRecord::Schema.define(version: 20150819074750) do
 
   create_table "administrative_areas", force: true do |t|
     t.string   "name",       null: false
