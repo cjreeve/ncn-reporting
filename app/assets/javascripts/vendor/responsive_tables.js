2d7d2de2c575f2e0fb6402735dfb12496cf1028c
Christopher Reeve <cjreeve@gmail.com>
2015-08-28 22:53:31 +0100

    fix responsive table to work with turbolinks

--- app/assets/javascripts/vendor/responsive_tables.js
+++ app/assets/javascripts/vendor/responsive_tables.js
@@ -1,69 +1,95 @@
-var updateTables;
+var tableSwitched = false;
 
 $(document).ready(function() {
-  var switched = false;
-  updateTables = function() {
-    if (($(window).width() < 767) && !switched ){
-      switched = true;
+
+  var updateTables = function() {
+    if (($(window).width() < 767) && !tableSwitched ){
+      tableSwitched = true;
       $("table.responsive").each(function(i, element) {
         splitTable($(element));
       });
       return true;
     }
-    else if (switched && ($(window).width() > 767)) {
-      switched = false;
+    else if (tableSwitched && ($(window).width() > 767)) {
+      tableSwitched = false;
       $("table.responsive").each(function(i, element) {
         unsplitTable($(element));
       });
     }
   };
-   
+
   $(window).load(updateTables);
-  $(window).on("redraw",function(){switched=false;updateTables();}); // An event to listen for
-  $(window).on("resize", updateTables);
-   
-	
-	function splitTable(original)
-	{
-		original.wrap("<div class='table-wrapper' />");
-		
-		var copy = original.clone();
-		copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
-		copy.removeClass("responsive");
-		
-		original.closest(".table-wrapper").append(copy);
-		copy.wrap("<div class='pinned' />");
-		original.wrap("<div class='scrollable' />");
 
-    setCellHeights(original, copy);
-	}
-	
-	function unsplitTable(original) {
-    original.closest(".table-wrapper").find(".pinned").remove();
-    original.unwrap();
-    original.unwrap();
-	}
+});
 
-  function setCellHeights(original, copy) {
-    var tr = original.find('tr'),
-        tr_copy = copy.find('tr'),
-        heights = [];
 
-    tr.each(function (index) {
-      var self = $(this),
-          tx = self.find('th, td');
+$(window).bind('page:change', function() {
 
-      tx.each(function () {
-        var height = $(this).outerHeight(true);
-        heights[index] = heights[index] || 0;
-        if (height > heights[index]) heights[index] = height;
+  tableSwitched = false;
+
+  var updateTables = function() {
+    if (($(window).width() < 767) && !tableSwitched ){
+      tableSwitched = true;
+      $("table.responsive").each(function(i, element) {
+        splitTable($(element));
+        console.log('here');
       });
+      return true;
+    }
+    else if (tableSwitched && ($(window).width() > 767)) {
+      tableSwitched = false;
+      $("table.responsive").each(function(i, element) {
+        unsplitTable($(element));
+      });
+    }
+  };
 
-    });
-
-    tr_copy.each(function (index) {
-      $(this).height(heights[index]);
-    });
+  if (!tableSwitched) {
+    updateTables();
   }
 
 });
+
+
+function splitTable(original)
+{
+  original.wrap("<div class='table-wrapper' />");
+
+  var copy = original.clone();
+  copy.find("td:not(:first-child), th:not(:first-child)").css("display", "none");
+  copy.removeClass("responsive");
+
+  original.closest(".table-wrapper").append(copy);
+  copy.wrap("<div class='pinned' />");
+  original.wrap("<div class='scrollable' />");
+
+  setCellHeights(original, copy);
+}
+
+function unsplitTable(original) {
+  original.closest(".table-wrapper").find(".pinned").remove();
+  original.unwrap();
+  original.unwrap();
+}
+
+function setCellHeights(original, copy) {
+  var tr = original.find('tr'),
+      tr_copy = copy.find('tr'),
+      heights = [];
+
+  tr.each(function (index) {
+    var self = $(this),
+        tx = self.find('th, td');
+
+    tx.each(function () {
+      var height = $(this).outerHeight(true);
+      heights[index] = heights[index] || 0;
+      if (height > heights[index]) heights[index] = height;
+    });
+
+  });
+
+  tr_copy.each(function (index) {
+    $(this).height(heights[index]);
+  });
+}
