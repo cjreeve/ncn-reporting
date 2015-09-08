c4a819fe7859fcc0b69f24ac416bf2ddcd44d2e5
Christopher Reeve <cjreeve@gmail.com>
2015-09-09 00:20:19 +0100

    enable subbmitted issue query

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,7 +2,13 @@ class SiteController < ApplicationController
   def notifications
     @user_draft_issue_count = Issue.where(user: current_user, state: 'draft').count
 
-    @user_submitted_issue_count = 0 #Issue.where(state: 'submitted', route: current_user.routes, area: current_user.areas).count
+    user_routes = current_user.routes
+    user_areas = current_user.areas
+    if user_routes.present? && user_areas.present?
+      @user_submitted_issue_count = Issue.where(state: 'submitted', route: user_routes, area: user_areas).count
+    else
+      @user_submitted_issue_count = 0
+    end
 
     @number_of_possitive_counters = [@user_draft_issue_count > 0, @user_submitted_issue_count > 0].count(true)
 
