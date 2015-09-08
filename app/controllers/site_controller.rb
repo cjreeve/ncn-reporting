7fcfacb65c26e448b83740baadb9013e30ef6f2a
Christopher Reeve <cjreeve@gmail.com>
2015-09-09 00:33:38 +0100

    fix for submitted issue count query

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,8 +2,8 @@ class SiteController < ApplicationController
   def notifications
     @user_draft_issue_count = Issue.where(user: current_user, state: 'draft').count
 
-    user_routes = current_user.routes
-    user_areas = current_user.areas
+    user_routes = current_user.routes.to_a
+    user_areas = current_user.areas.to_a
     if user_routes.present? && user_areas.present?
       @user_submitted_issue_count = Issue.where(state: 'submitted', route: user_routes, area: user_areas).count
     else
