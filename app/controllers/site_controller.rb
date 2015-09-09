bc3b6ed93fbc06d57d6495a8d9bed67ff52f1798
Christopher Reeve <cjreeve@gmail.com>
2015-09-09 22:24:14 +0100

    add resolved and own resolved notifications

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,18 +1,35 @@
 class SiteController < ApplicationController
   def notifications
-    user_routes = current_user.routes.to_a
-    user_areas = current_user.areas.to_a
+    @user_routes = current_user.routes.to_a
+    @user_areas = current_user.areas.to_a
+    has_routes = @user_routes.present? && @user_areas.present?
 
 
     @user_draft_issue_count = Issue.where(user: current_user, state: 'draft').count
 
     @user_submitted_issue_count = 0
-    if %w{ranger admin}.include?(current_user.role) && user_routes.present? && user_areas.present?
-      @user_submitted_issue_count = Issue.where(state: 'submitted', route: user_routes, area: user_areas).count
+    if %w{ranger admin}.include?(current_user.role) && has_routes
+      @user_submitted_issue_count = Issue.where(state: 'submitted', route: @user_routes, area: @user_areas).count
     end
 
-    @number_of_possitive_counters = [@user_draft_issue_count > 0, @user_submitted_issue_count > 0].count(true)
-    @total_pending_count = @user_draft_issue_count + @user_submitted_issue_count
+    @user_resolved_issue_count = 0
+    if %w{ranger admin}.include?(current_user.role) && has_routes
+      @user_resolved_issue_count = Issue.where(state: 'resolved', route: @user_routes, area: @user_areas).count
+    end
+
+    @own_issue_resolved_count = Issue.where(state: 'resolved', user: current_user).count
+
+    @number_of_possitive_counters = [
+      @user_draft_issue_count > 0,
+      @user_submitted_issue_count > 0,
+      @user_resolved_issue_count > 0,
+      @own_issue_resolved_count > 0
+    ].count(true)
+
+    @total_pending_count =  @user_draft_issue_count +
+                            @user_submitted_issue_count +
+                            @user_resolved_issue_count +
+                            @own_issue_resolved_count
 
     render layout: false
   end
