21df129ca96b4632e46cb052fbd51755123a6a9a
Christopher Reeve <cjreeve@gmail.com>
2015-09-09 21:50:36 +0100

    remove unecessary if

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,18 +1,17 @@
 class SiteController < ApplicationController
   def notifications
+    user_routes = current_user.routes.to_a
+    user_areas = current_user.areas.to_a
+
+
     @user_draft_issue_count = Issue.where(user: current_user, state: 'draft').count
 
     @user_submitted_issue_count = 0
-    if %w{ranger admin}.include?(current_user.role)
-      user_routes = current_user.routes.to_a
-      user_areas = current_user.areas.to_a
-      if user_routes.present? && user_areas.present?
-        @user_submitted_issue_count = Issue.where(state: 'submitted', route: user_routes, area: user_areas).count
-      end
+    if %w{ranger admin}.include?(current_user.role) && user_routes.present? && user_areas.present?
+      @user_submitted_issue_count = Issue.where(state: 'submitted', route: user_routes, area: user_areas).count
     end
 
     @number_of_possitive_counters = [@user_draft_issue_count > 0, @user_submitted_issue_count > 0].count(true)
-
     @total_pending_count = @user_draft_issue_count + @user_submitted_issue_count
 
     render layout: false
