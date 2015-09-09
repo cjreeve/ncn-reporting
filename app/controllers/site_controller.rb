9b243c0f156b1d55421ffe92bbe94420485faa02
Christopher Reeve <cjreeve@gmail.com>
2015-09-09 21:47:24 +0100

    restrict submitted issue notifications only to ranger and admin

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,12 +2,13 @@ class SiteController < ApplicationController
   def notifications
     @user_draft_issue_count = Issue.where(user: current_user, state: 'draft').count
 
-    user_routes = current_user.routes.to_a
-    user_areas = current_user.areas.to_a
-    if user_routes.present? && user_areas.present?
-      @user_submitted_issue_count = Issue.where(state: 'submitted', route: user_routes, area: user_areas).count
-    else
-      @user_submitted_issue_count = 0
+    @user_submitted_issue_count = 0
+    if %w{ranger}.include?(current_user.role)
+      user_routes = current_user.routes.to_a
+      user_areas = current_user.areas.to_a
+      if user_routes.present? && user_areas.present?
+        @user_submitted_issue_count = Issue.where(state: 'submitted', route: user_routes, area: user_areas).count
+      end
     end
 
     @number_of_possitive_counters = [@user_draft_issue_count > 0, @user_submitted_issue_count > 0].count(true)
