e54ae69b7e117f60b0bb493f1ce9ca5cebaf5b1c
Christopher Reeve <cjreeve@gmail.com>
2015-10-04 19:23:26 +0100

    try to fix s3 credentials

--- app/uploaders/site_uploader.rb
+++ app/uploaders/site_uploader.rb
@@ -4,10 +4,10 @@ class SiteUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   include Sprockets::Rails::Helper # not sure if it is required
 
-  if Rails.env == 'development'
-    storage :file
-  else
+  # if Rails.env == 'development'
+    # storage :file
+  # else
     storage :fog
-  end
+  # end
 
 end
