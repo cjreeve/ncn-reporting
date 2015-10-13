dbcc1af4de3fb8f14ad35e26756d20802277635a
Christopher Reeve <cjreeve@gmail.com>
2015-10-13 22:50:44 +0100

    copy administrative_area short name to short_name to get around that database was set that it is not unique so can't be all nil or an empty string

--- app/models/area.rb
+++ app/models/area.rb
@@ -6,4 +6,10 @@ class Area < ActiveRecord::Base
   has_many :users, through: :user_managed_area_selections
 
   has_many :administrative_areas
+
+  before_validation :set_blank_short_name
+
+  def set_blank_short_name
+    self.short_name = self.name unless self.short_name.present?
+  end
 end
