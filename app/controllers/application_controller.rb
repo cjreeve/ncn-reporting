1b287245afe1ace48060357cc3cae96515090545
Christopher Reeve <cjreeve@gmail.com>
2015-11-02 22:53:17 +0000

    redirect users back to the url they were going to when they where not logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,29 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
-
   before_filter :log_user_activity
+
+  protect_from_forgery# with: :exception
+
+  after_filter :store_location
+
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    return unless request.get? 
+    if (request.path != "/users/sign_in" &&
+        request.path != "/users/sign_up" &&
+        request.path != "/users/password/new" &&
+        request.path != "/users/password/edit" &&
+        request.path != "/users/confirmation" &&
+        request.path != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath 
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
 
   rescue_from CanCan::AccessDenied do |exception|
     account_locked = nil
@@ -15,6 +35,7 @@ class ApplicationController < ActionController::Base
       end
       redirect_url_ = '/'
     else
+      store_location()
       redirect_url_ = user_session_url
     end
     redirect_to redirect_url_, alert: (account_locked ? account_locked : exception.message)
