3ac92191185345ec054045a1ea865d329caeea6d
Christopher Reeve <cjreeve@gmail.com>
2015-11-14 20:24:23 +0000

    allow formated comments on updates page

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :issue
+
+  def formatted_content
+    ActionController::Base.helpers.strip_tags(
+      self.content).gsub(/(?:\n\r?|\r\n?)/, '<br>').split('<br><br>').reject(&:empty?).join('<br><br>').html_safe
+  end
 end
