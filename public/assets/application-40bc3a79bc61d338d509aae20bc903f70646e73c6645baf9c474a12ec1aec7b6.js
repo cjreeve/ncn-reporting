531dc78104844e27c128670d2043a6ed452a10dc
Christopher Reeve <cjreeve@gmail.com>
2015-11-17 23:25:23 +0000

    assets

--- public/assets/application-40bc3a79bc61d338d509aae20bc903f70646e73c6645baf9c474a12ec1aec7b6.js
+++ public/assets/application-40bc3a79bc61d338d509aae20bc903f70646e73c6645baf9c474a12ec1aec7b6.js
@@ -19020,13 +19020,18 @@ function showMyCoord() {
         touchDrag: true
       });
 
-      $("#issue-modal-images").owlCarousel({
+      var issueModalImages = $("#issue-modal-images").owlCarousel({
         singleItem: true,
         navigation: true,
         navigationText: false,
         pagination: true,
         mouseDrag: true,
         touchDrag: true
+      });
+
+      $('#issue-page-images .issue-image-container img').on('click', function(){
+        var index = $(this).data('index');
+        issueModalImages.trigger('owl.goTo', index);
       });
 
     });
