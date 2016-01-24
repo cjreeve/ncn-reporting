e7cfb063c9ced529eddaeb1ed14d56875f60020a
Christopher Reeve <cjreeve@gmail.com>
2016-01-24 23:13:45 +0000

    undo carrier dev test settings

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,7 @@
 # encoding: UTF-8
 
 CarrierWave.configure do |config|
-  if Rails.env == 'development'
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: '',
-      aws_secret_access_key: '',
-      region: 'eu-west-1'
-    }
-    config.fog_directory = 'ncn-reporting'
-  else
+  unless Rails.env == 'development'
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
