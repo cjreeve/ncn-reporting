008f490993319e10a06e6bbe6bc9b8e589684923
Christopher Reeve <cjreeve@gmail.com>
2016-03-31 23:46:24 +0100

    turn off geocoder settings that are not working

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,19 +1,19 @@
 Geocoder.configure(
   # Geocoding options
-  # timeout: 3,                 # geocoding service timeout (secs)
-  lookup: :google,              # name of geocoding service (symbol)
+  # timeout: 15,                 # geocoding service timeout (secs)
+  # lookup: :google,              # name of geocoding service (symbol)
   # language: :en,              # ISO-639 language code
-  use_https: true,              # use HTTPS for lookup requests? (if supported)
+  # use_https: true,              # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: ENV['GOOGLEMAPS_API_KEY']   # API key for geocoding service
+  # api_key: 'AIzaSyBFm3Yq2z3csVzGGQe_qlKMrMJ8RxdnxiI',   # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #keys)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
   # Exceptions that should not be rescued by default
   # (if you want to implement custom error handling);
   # supports SocketError and TimeoutError
-  # always_raise: [],
+  # always_raise: :all
 
   # Calculation options
   # units: :mi,                 # :km for kilometers or :mi for miles
