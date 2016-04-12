c826f62500ddb9693a98e1dd93660fa530bf312c
Christopher Reeve <cjreeve@gmail.com>
2016-04-12 22:24:26 +0100

    add 'new' tooltip and a cancel link

--- app/assets/javascripts/followers.js
+++ app/assets/javascripts/followers.js
@@ -12,6 +12,12 @@ function watch_followers_edit_click() {
     $('#follower-list').hide();
     $('#follower-tokens').fadeIn();
   });
+
+  $('#cancel-issue-follower-update').click(function(e) {
+    e.preventDefault();
+    $('#follower-tokens').hide();
+    $('#follower-list').fadeIn();
+  });
 }
 
 
