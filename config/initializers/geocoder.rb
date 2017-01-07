80e2f8e4fb64b6f5c322349e101e610ce1b67e47
Christopher Reeve <cjreeve@gmail.com>
2017-01-07 22:02:41 +0000

    switch back to updated geocoder and unstricted key

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -6,7 +6,11 @@ Geocoder.configure(
   use_https: true,              # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: "#{ Rails.application.config.google_api_key }" #,   # API key for geocoding service
+
+  # api_key: "#{ Rails.application.config.google_api_key }" #,   # API key for geocoding service
+  # TODO - get gecoder working with heroku and restricted api key
+  api_key: 'AIzaSyBFm3Yq2z3csVzGGQe_qlKMrMJ8RxdnxiI'
+
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #keys)
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
