f807a96696856e913af74844f42d1e8fd43667d6
Christopher Reeve <cjreeve@gmail.com>
2017-10-26 19:39:29 +0100

    try to stop error occuring when logged out

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -5,32 +5,35 @@ class SiteController < ApplicationController
   end
 
   def notifications
-    redirect_to root_path unless current_user
-    @user = current_user
+    if current_user
+      @user = current_user
 
-    #################### shared with email notifier ###################
-    counter_array = []
-    user_routes = @user.routes.to_a
-    @user_route_slugs = user_routes.collect(&:slug).join('.')
+      #################### shared with email notifier ###################
+      counter_array = []
+      user_routes = @user.routes.to_a
+      @user_route_slugs = user_routes.collect(&:slug).join('.')
 
-    user_areas = @user.administrative_areas.to_a
-    @user_area_ids = user_areas.collect(&:id).join('.')
-    user_labels = @user.labels
-    @label_ids = Hash[ user_labels.collect { |l| [l.name.parameterize.to_sym, l.id] } ]
+      user_areas = @user.administrative_areas.to_a
+      @user_area_ids = user_areas.collect(&:id).join('.')
+      user_labels = @user.labels
+      @label_ids = Hash[ user_labels.collect { |l| [l.name.parameterize.to_sym, l.id] } ]
 
-    @total_pending_count = [
-      @own_draft_issue_count = @user.own_draft_issue_count,
-      @own_issue_resolved_count = @user.own_issue_resolved_count,
-      @own_issue_unsolvable_count = @user.own_issue_unsolvable_count,
-      @user_submitted_issue_count = @user.user_submitted_issue_count(user_areas, user_routes),
-      @user_resolved_issue_count = @user.user_resolved_issue_count(user_areas, user_routes)
-    ].sum
+      @total_pending_count = [
+        @own_draft_issue_count = @user.own_draft_issue_count,
+        @own_issue_resolved_count = @user.own_issue_resolved_count,
+        @own_issue_unsolvable_count = @user.own_issue_unsolvable_count,
+        @user_submitted_issue_count = @user.user_submitted_issue_count(user_areas, user_routes),
+        @user_resolved_issue_count = @user.user_resolved_issue_count(user_areas, user_routes)
+      ].sum
 
-    @open_label_counts = @user.open_label_counts(user_labels, user_areas, user_routes)
-    @total_pending_count += @open_label_counts.values.sum
-    ####################################################################
+      @open_label_counts = @user.open_label_counts(user_labels, user_areas, user_routes)
+      @total_pending_count += @open_label_counts.values.sum
+      ####################################################################
 
-    render layout: false
+      render layout: false
+    else
+      redirect_to root_path
+    end
   end
 
 
@@ -51,11 +54,14 @@ class SiteController < ApplicationController
 
 
   def updates_count
-    redirect_to root_path unless current_user
-    last_visit = current_user.visited_updates_at
-    @updates_count = Issue.where('updated_at > ?', last_visit).count
-    @updates_count += Page.where('updated_at > ?', last_visit).count
-    render layout: false
+    if current_user
+      last_visit = current_user.visited_updates_at
+      @updates_count = Issue.where('updated_at > ?', last_visit).count
+      @updates_count += Page.where('updated_at > ?', last_visit).count
+      render layout: false
+    else
+      redirect_to root_path
+    end
   end
 
   def mini_search
