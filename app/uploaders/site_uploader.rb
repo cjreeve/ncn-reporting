336d32ffe3c9c6e2ba505f7899132e460ebc8c8d
Christopher Reeve <cjreeve@gmail.com>
2019-05-06 16:02:52 +0100

    disable removing of files unless production

--- app/uploaders/site_uploader.rb
+++ app/uploaders/site_uploader.rb
@@ -4,10 +4,15 @@ class SiteUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
   include Sprockets::Rails::Helper # not sure if it is required
 
-  if Rails.env == 'development'
+  if Rails.env.development?
     storage :file
   else
     storage :fog
+  end
+
+  unless Rails.env.production?
+    def remove!
+    end
   end
 
 end
