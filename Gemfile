1967723cd9561a7b3d5c74e4bd8dcddede4ba646
Christopher Reeve <cjreeve@gmail.com>
2020-02-19 22:36:10 +0000

    try adding the puma server again

--- Gemfile
+++ Gemfile
@@ -4,7 +4,7 @@ source 'https://rubygems.org'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '5.2.2'
 ruby "2.4.1"
-# gem 'puma'
+gem 'puma'
 
 # gem 'active_record_union', '~> 1.3'
 
@@ -88,7 +88,6 @@ end
 
 group :production do
   gem 'pg', '>= 0.18', '< 2.0'
-  gem 'rails_12factor' #, '~> 0.0'
 end
 
 # Use ActiveModel has_secure_password
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,14 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
+
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_files = true
 
@@ -35,7 +43,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # required by devise
-  config.assets.initialize_on_precompile = false
+  # config.assets.initialize_on_precompile = false
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
